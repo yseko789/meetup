a73c8b69150242cf924a570c38d8c7f1106c24c4
Yuta Seko <yseko@usc.edu>
2022-07-13 23:32:57 -0700

    updated to change ways to edit vendor data

--- controllers/vendor.js
+++ controllers/vendor.js
@@ -6,28 +6,50 @@ const getVendor = async(req, res)=>{
     const vendor = await Vendor.findById(
         req.params.id
     )
-
     if(!vendor){
         throw new Error('Vendor does not exist.')
     }
-    res.status(StatusCodes.OK).json(vendor)
+
+    const user = await User.findById(req.user.userId)
+    const userExists = await user.vendors.includes(req.params.id)
+    let userStatus = ""
+    if(!userExists){
+        userStatus = "none"
+    }else{
+        if(vendor.createdById == req.user.userId){
+            userStatus = "host"
+        }else{
+            userStatus = "attendee"
+        }
+    }
+    res.status(StatusCodes.OK).json({vendor, userStatus})
 }
 
 const updateVendor = async(req, res)=>{
     const vendor = await Vendor.findByIdAndUpdate(
         req.params.id,
-        req.body,
+        req.body.vendorInfo,
         {new: true}
     )
     if(!vendor){
         throw new Error('Vendor does not exist.')
     }
+
+    //have to change this depending on whether 'attendee' or 'none'
     if(vendor.createdBy!=req.user.userId){
-        const user = await User.findByIdAndUpdate(
-            req.user.userId,
-            {$push: {"vendors":vendor}},
-            {new:true}
-        )
+        if(req.body.userStatus === "none"){
+            const user = await User.findByIdAndUpdate(
+                req.user.userId,
+                {$push: {"vendors":vendor}},
+                {new:true}
+            )
+        }else if(req.body.userStatus === "attendee"){
+            const user = await User.findByIdAndUpdate(
+                req.user.userId, 
+                {$pull: {"vendors": vendor}},
+                {safe: true, multi:false}
+            )
+        }
     }
     res.status(StatusCodes.OK).json(vendor)
 }
